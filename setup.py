0ccb2c71d6a979f29915ee93c6d1fed2d07a111e
BDAVIDOV91 <bgtscerebrate@gmail.com>
2024-05-31 20:27:57 +0300

    add configuration to project

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
 setup(
-    name='python-boilerplate',
+    name='subscription-service-backend',
     version='1.0',
     author='Blagovest Davidov',
     description='A sample Python project',
     install_requires=[
         'flake8==7.0.0',
         'autopep8==2.1.0',
-        'pytest==8.1.2'
+        'pytest==8.1.2',
+        'Flask==2.0.3',  # For Flask-based setup
+        'Flask-SQLAlchemy==2.5.1',
+        'Flask-Migrate==3.1.0',
+        'Flask-JWT-Extended==4.4.3',
+        'psycopg2-binary==2.9.3',
+        'marshmallow==3.14.1',
+        'celery==5.2.3',
+        'wheel==0.43.0',
+        'python-dotenv==1.0.1'
     ]
 )
---
+++ setup_local.sh
@@ -0,0 +1,10 @@
+export POSTGRES_USER=bgts
+export POSTGRES_PASS=bgts1337
+export POSTGRES_DB=default
+export POSTGRES_PORT=5432
+docker stop $(docker ps -a -q)
+docker container prune
+docker volume prune
+docker run -d -p 5432:5432 --name my-postgres -e POSTGRES_USER=bgts -e POSTGRES_PASSWORD=bgts1337 -e POSTGRES_DB=default postgres:16
+docker run -d -p 80:80 --name my-pgadmin -e PGADMIN_DEFAULT_EMAIL=bgts@bgts.com -e PGADMIN_DEFAULT_PASSWORD=bgts1337 dpage/pgadmin4
+echo "running"
